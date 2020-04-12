ba0c593f37656d13178c36966712dc22c958ed3c
Samatar <samatar_267@hotmail.com>
2020-04-12 20:32:26 +0100

    Add circleCI deploy service account

--- iam.tf
+++ iam.tf
@@ -4,9 +4,21 @@ resource "google_service_account" "terraform" {
   display_name = "Terraform SA"
 }
 
+resource "google_service_account" "circle_deploy" {
+  project      = "${google_project.samatar_dev.project_id}"
+  account_id   = "circleci_deploy"
+  display_name = "CircleCI Deploy"
+}
+
 resource "google_project_iam_member" "terraform_owner" {
   project = "${google_project.samatar_dev.project_id}"
   role    = "roles/owner"
   member  = "serviceAccount:${google_service_account.terraform.email}"
 }
 
+
+resource "google_project_iam_member" "circleci_deploy" {
+  project = "${google_project.samatar_dev.project_id}"
+  role    = "roles/storage.objectViewer"
+  member  = "serviceAccount:${google_service_account.circleci_deploy.email}"
+}
