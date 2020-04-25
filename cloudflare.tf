ea3421f437d175f529ccc7e0c41dfcd8ed3ecbf6
Samatar <samatar_267@hotmail.com>
2020-04-25 17:39:59 +0100

    Add design gcs and cloudflare record

--- cloudflare.tf
+++ cloudflare.tf
@@ -6,8 +6,8 @@ resource "cloudflare_zone" "samatar" {
   zone = "samatar.dev"
 }
 
-resource "cloudflare_" "name" {
-  zone_id = "${cloudflare_zone.samatar.zone_id}"
+resource "cloudflare_record" "design" {
+  zone_id = "${cloudflare_zone.samatar.id}"
   name    = "design"
   value   = "c.storage.googleapis.com"
   type    = "CNAME"
