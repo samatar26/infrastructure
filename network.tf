876eea4129483ae959c291cb759d96e588749966
Samatar <Samatar_267@hotmail.com>
2020-07-26 21:24:59 +0100

    Set project as I'm not using provider :sweat-smile: (#9)

--- network.tf
+++ network.tf
@@ -1,4 +1,5 @@
 resource "google_compute_network" "vpc_network" {
+  project                 = "${google_project.samatar_dev.project_id}"
   name                    = "kubernetes-the-hard-way"
   auto_create_subnetworks = false
 }
