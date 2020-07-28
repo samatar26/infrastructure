477e2c7c23e52be0271ec819c0d5e5114cbfa849
Samatar <samatar_267@hotmail.com>
2020-07-28 21:38:53 +0100

    Setup kubernetes-the-hard-way subnet

--- network.tf
+++ network.tf
@@ -1,5 +1,11 @@
 resource "google_compute_network" "vpc_network" {
   project                 = "${google_project.samatar_dev.project_id}"
   name                    = "kubernetes-the-hard-way"
-  auto_create_subnetworks = false
+  auto_create_subnetworks = false # We want to create a custom network so we can set a large enough IP address range, to assign a private IP address to each node in the Kubernetes cluster
+}
+
+resource "google_compute_subnetwork" "kubernetes-the-hard-way-subnet" {
+  name          = "kubernetes-cluster-subnet"
+  network       = google_compute_network.vpc_network.id
+  ip_cidr_range = "10.240.0.0/24"
 }
