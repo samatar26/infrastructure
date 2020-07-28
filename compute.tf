c2e32616384c45d4852e50ff62da3718762a36f3
Samatar <Samatar_267@hotmail.com>
2020-07-28 23:38:37 +0100

    Add 3 compute instances to host Kubernetes controllers (#15)
    * Add 3 compute instances to host Kubernetes controllers
    
    * Set name and zone for compute instances

---
+++ compute.tf
@@ -0,0 +1,37 @@
+resource "google_compute_address" "kubernetes_the_hard_way" {
+  name = "kubernetes-public-api-server"
+
+  region = "europe-west3"
+}
+
+
+# Create 3 compute instances for the control plane
+
+resource "google_compute_instance" "controller" {
+  count = 3
+
+  can_ip_forward = true
+  name           = "kubernetes-controller-${count.index}"
+  zone           = "europe-west3-a"
+
+  boot_disk {
+    initialize_params {
+      size  = 200
+      image = "ubuntu-2004-lts"
+
+    }
+  }
+
+  network_interface {
+    subnetwork = "kubernetes-cluster-subnet"
+    network_ip = "10.240.0.1${count.index}"
+  }
+
+
+
+  machine_type = "e2-standard-2"
+  tags         = ["kubernetes-the-hard-way", "controller"]
+
+  #Question haven't defined scopes, will need to see if needed later on:
+  # compute-rw,storage-ro,service-management,service-control,logging-write,monitoring
+}
