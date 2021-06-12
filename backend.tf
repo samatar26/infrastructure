e650201836de32e724fa0df0fe1f6d019df28be8
Samatar <axmedsamatar.dev@gmail.com>
2021-06-12 20:46:24 +0100

    Update terraform to 1.0

--- backend.tf
+++ backend.tf
@@ -2,5 +2,11 @@ terraform {
   backend "gcs" {
     bucket = "samatar-tf-state"
   }
-}
 
+  required_providers {
+        cloudflare = {
+            source = "registry.terraform.io/cloudflare/cloudflare"
+            version = "2.18.0"
+        }
+    }
+}
--- cloudflare.tf
+++ cloudflare.tf
@@ -1,9 +1,3 @@
-provider "cloudflare" {
-  version = "~> 2.0"
-}
-
-
-
 resource "cloudflare_zone" "samatar" {
   zone = "samatar.dev"
 }
