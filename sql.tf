4fdd32302f0430021ff41a86554df810521a0a58
Samatar <axmedsamatar.dev@gmail.com>
2021-06-13 10:14:22 +0100

    Add anime postgres db

---
+++ sql.tf
@@ -0,0 +1,11 @@
+resource "google_sql_database_instance" "anime" {
+
+  name             = "anime"
+  database_version = "POSTGRES_12"
+  region           = "europe-west2"
+
+  settings {
+    tier = "db-custom-1-3840"
+  }
+
+}
