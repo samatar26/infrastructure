df41d12b6a8266305eb80df38b461aac4eb948cb
Samatar <axmedsamatar.dev@gmail.com>
2021-06-14 12:41:44 +0100

    Add anime cloud run service

--- iam.tf
+++ iam.tf
@@ -8,6 +8,11 @@ resource "google_service_account" "circleci_deploy" {
   display_name = "CircleCI Deploy SA"
 }
 
+resource "google_service_account" "anime_api_cloud_run" {
+  account_id   = "anime-api"
+  display_name = "Cloud run anime api SA"
+}
+
 resource "google_project_iam_member" "terraform_owner" {
   role   = "roles/owner"
   member = "serviceAccount:${google_service_account.terraform.email}"
@@ -17,4 +22,10 @@ resource "google_project_iam_member" "terraform_owner" {
 resource "google_project_iam_member" "circleci_deploy_registry" {
   role   = "roles/storage.admin"
   member = "serviceAccount:${google_service_account.circleci_deploy.email}"
+}
+
+
+resource "google_project_iam_member" "cloud_sql_client" {
+  role   = "roles/cloudsql.client"
+  member = "serviceAccount:${google_service_account.anime_api_cloud_run.email}"
 }
