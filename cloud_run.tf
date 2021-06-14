2d0c2e042c7f11083d811708042b7ca5df397c60
Samatar <axmedsamatar.dev@gmail.com>
2021-06-14 12:45:44 +0100

    Add anime cloud run service (#23)
    * Add anime cloud run service
    
    * Fix cloudsql instance reference

---
+++ cloud_run.tf
@@ -0,0 +1,25 @@
+resource "google_cloud_run_service" "anime-api" {
+  name     = "anime"
+  location = "europe-west3"
+
+  template {
+    spec {
+      containers {
+        image = "eu.gcr.io/samatar-dev-43f2d25b/anime-api"
+      }
+
+      service_account_name = google_service_account.anime_api_cloud_run.email
+    }
+
+    metadata {
+      annotations = {
+        "run.googleapis.com/cloudsql-instances" = google_sql_database_instance.anime.name
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
