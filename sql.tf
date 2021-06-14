0df5b9efffefe6a6416391774c60cb52faa405fa
Samatar <axmedsamatar.dev@gmail.com>
2021-06-14 15:49:09 +0100

    Add forests cloud sql instance and db

--- sql.tf
+++ sql.tf
@@ -22,3 +22,29 @@ resource "google_sql_user" "anime" {
   instance = google_sql_database_instance.anime.name
   password = var.anime_db_password
 }
+
+
+resource "google_sql_database_instance" "forests" {
+
+  name             = "forests"
+  database_version = "POSTGRES_12"
+  region           = "europe-west2"
+
+  settings {
+    tier = "db-custom-1-3840"
+  }
+
+}
+
+resource "google_sql_database" "forests" {
+  name     = "forests"
+  instance = google_sql_database_instance.forests.name
+}
+
+variable "forests_db_password" {}
+
+resource "google_sql_user" "forests" {
+  name     = "forests"
+  instance = google_sql_database_instance.forests.name
+  password = var.forests_db_password
+}
